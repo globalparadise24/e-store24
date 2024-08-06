5b1f36e1cce776ee2c92a104d30f8b37ee566683
globalparadise24 <global.parardise.24@gmail.com>
2024-08-06 16:22:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
 // MongoDB connection
-const uri = "mongodb+srv://globalparadise24:<password>@cluster0.lbw3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect('mongodb+srv://globalparardise24:<1jKnnVMSujKOtbfD>@cluster0.lbw3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (err) {
-    console.error("Failed to connect to MongoDB", err);
-  }
-}
-run().catch(console.dir);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,4 +23,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
